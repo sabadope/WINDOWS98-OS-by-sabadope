d3b8892dcf3c8b86b65393b580814f3120a52128
FERTONY <sabado.f.bscs@gmail.com>
2025-02-20 11:16:42 +0800

    Updated Files using Linux

--- index.php
+++ index.php
@@ -1,138 +1,142 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
+  <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="output.css" />
     <script type="module" src="./script.js" defer></script>
     <title>Document</title>
-</head>
+    <style>
+    .wallpaper {
+      height: 100vh;
+      width: 100vw;
+      position: fixed;
+      background-size: cover;
+      -webkit-user-drag: none;
+      background-image: url("./assets/wallpaper/windows-98.jpg");
+      background-position: center;
+    }
 
-<body>
+
+    </style>
+  </head>
+
+  <body>
     <div class="wallpaper"></div>
-
     <div class="task-bar">
-        <button class="task-bar__start">
-            <img class="task-bar__start--img" src="./assets/icons/start.png" alt="" />
-            <span class="task-bar__start--text">Start</span>
-        </button>
-        <div class="task-bar__apps"></div>
-        <div class="task-bar__clock">00:00:00 AM</div>
+      <button class="task-bar__start">
+        <img class="task-bar__start--img" src="./assets/icons/start.png" alt="" />
+        <span class="task-bar__start--text">Start</span>
+      </button>
+      <div class="task-bar__apps"></div>
+      <div class="task-bar__clock">00:00:00 AM</div>
     </div>
     <div class="start-menu">
-        <div class="start-menu__side-banner">
-            <span class="start-menu__side-banner--text">Windows 98</span>
-        </div>
-        <div class="start-menu__apps">
-            <div class="start-menu__app apps__notepad">
-                <img class="start-menu__app--icon apps__notepad--icon" src="./assets/icons/notepad.png" alt="notepad" /><span class="start-menu__app--icon--text apps__notepad--text">Notepad</span
-          >
+      <div class="start-menu__side-banner">
+        <span class="start-menu__side-banner--text">Windows 98</span>
+      </div>
+      <div class="start-menu__apps">
+        <div class="start-menu__app apps__notepad">
+          <img class="start-menu__app--icon apps__notepad--icon" src="./assets/icons/notepad.png" alt="notepad" />
+          <span class="start-menu__app--icon--text apps__notepad--text">Notepad</span>
         </div>
         <div class="start-menu__app apps__address-book">
-          <img
-            class="start-menu__app--icon apps__address-book--icon"
-            src="./assets/icons/address_book.png"
-            alt="address-book"
-          /><span class="start-menu__app--icon--text apps__address-book--text"
-            >Address Book
-          </span>
-            </div>
-            <div class="start-menu__app apps__paint">
-                <img class="apps__paint--icon start-menu__app--icon" src="./assets/icons/paint.png" alt="paint" /><span class="start-menu__app--icon--text apps__paint--text">Paint
-          </span>
-            </div>
+          <img class="start-menu__app--icon apps__address-book--icon" src="./assets/icons/address_book.png" alt="address-book" />
+          <span class="start-menu__app--icon--text apps__address-book--text">Address Book </span>
         </div>
+        <div class="start-menu__app apps__paint">
+          <img class="apps__paint--icon start-menu__app--icon" src="./assets/icons/paint.png" alt="paint" />
+          <span class="start-menu__app--icon--text apps__paint--text">Paint </span>
+        </div>
+      </div>
     </div>
     <div class="desktop">
-        <div class="desktop__icon desktop__icon--notepad">
-            <img class="desktop__icon--img" src="./assets/icons/notepad.png" alt="notepad-icon" />
-            <div class="desktop__icon--text">Notepad</div>
-        </div>
-        <div class="desktop__icon desktop__icon--address_book">
-            <img class="desktop__icon--img" src="./assets/icons/address_book.png" alt="notepad-icon" />
-            <div class="desktop__icon--text">Address Book</div>
-        </div>
-        <div class="desktop__icon desktop__icon--paint">
-            <img class="desktop__icon--img" src="./assets/icons/paint.png" alt="notepad-icon" />
-            <div class="desktop__icon--text">Paint</div>
-        </div>
+      <div class="desktop__icon desktop__icon--notepad">
+        <img class="desktop__icon--img" src="./assets/icons/notepad.png" alt="notepad-icon" />
+        <div class="desktop__icon--text">Notepad</div>
+      </div>
+      <div class="desktop__icon desktop__icon--address_book">
+        <img class="desktop__icon--img" src="./assets/icons/address_book.png" alt="notepad-icon" />
+        <div class="desktop__icon--text">Address Book</div>
+      </div>
+      <div class="desktop__icon desktop__icon--paint">
+        <img class="desktop__icon--img" src="./assets/icons/paint.png" alt="notepad-icon" />
+        <div class="desktop__icon--text">Paint</div>
+      </div>
     </div>
     <div class="app app__notepad">
-        <div class="app__header-bar app__notepad--header-bar">
-            <img class="app__header-bar--icon" src="./assets/icons/notepad.png" alt="" />
-            <span class="app__header-bar--title">Notepad</span>
-            <button class="app__header-bar--close">
-                <img src="./assets/icons/close-icon.png" alt="" />
-            </button>
-        </div>
-        <div class="app__utility">
-            <button class="app__utility--btn app__utility--save">Save</button>
-            <button class="app__utility--btn app__utility--delete">Delete</button>
-        </div>
-        <div class="notepad">
-            <textarea class="notepad__textarea" name="" id="" cols="30" rows="10"></textarea>
-        </div>
+      <div class="app__header-bar app__notepad--header-bar">
+        <img class="app__header-bar--icon" src="./assets/icons/notepad.png" alt="" />
+        <span class="app__header-bar--title">Notepad</span>
+        <button class="app__header-bar--close">
+          <img src="./assets/icons/close-icon.png" alt="" />
+        </button>
+      </div>
+      <div class="app__utility">
+        <button class="app__utility--btn app__utility--save">Save</button>
+        <button class="app__utility--btn app__utility--delete">Delete</button>
+      </div>
+      <div class="notepad">
+        <textarea class="notepad__textarea" name="" id="" cols="30" rows="10"></textarea>
+      </div>
     </div>
     <div class="app app__address_book">
-        <div class="app__header-bar app__address_book--header-bar">
-            <img class="app__header-bar--icon" src="./assets/icons/address_book.png" alt="" />
-            <span class="app__header-bar--title">Address_Book</span>
-            <button class="app__header-bar--close">
-                <img src="./assets/icons/close-icon.png" alt="" />
-            </button>
-        </div>
-        <div class="app__utility">
-            <form class="address_book__form">
-                <label for="name">Name:</label>
-                <input id="name" class="address_book__form--input" reqiured name="name" size="5" type="text" />
-                <label for="tel">Tel:</label>
-                <input id="tel" class="address_book__form--input" reqiured name="tel" size="6" type="tel" pattern="[0-9]{10}" />
-                <label for="address">Address:</label>
-                <input id="address" class="address_book__form--input" reqiured name="address" size="15" type="text" />
-
-                <button type="submit" class="address_book__form--submit">Add</button>
-            </form>
-        </div>
-        <div class="address_book">
-            <table class="address_book__table">
-                <tr>
-                    <th class="address_book__table--header">Name</th>
-                    <th class="address_book__table--header">Phone</th>
-                    <th class="address_book__table--header">Address</th>
-                </tr>
-                <tr>
-                    <td class="address_book__table--name">Name</td>
-                    <td class="address_book__table--tel">Phone</td>
-                    <td class="address_book__table--address">Address</td>
-                </tr>
-            </table>
-        </div>
+      <div class="app__header-bar app__address_book--header-bar">
+        <img class="app__header-bar--icon" src="./assets/icons/address_book.png" alt="" />
+        <span class="app__header-bar--title">Address_Book</span>
+        <button class="app__header-bar--close">
+          <img src="./assets/icons/close-icon.png" alt="" />
+        </button>
+      </div>
+      <div class="app__utility">
+        <form class="address_book__form">
+          <label for="name">Name:</label>
+          <input id="name" class="address_book__form--input" reqiured name="name" size="5" type="text" />
+          <label for="tel">Tel:</label>
+          <input id="tel" class="address_book__form--input" reqiured name="tel" size="6" type="tel" pattern="[0-9]{10}" />
+          <label for="address">Address:</label>
+          <input id="address" class="address_book__form--input" reqiured name="address" size="15" type="text" />
+          <button type="submit" class="address_book__form--submit">Add</button>
+        </form>
+      </div>
+      <div class="address_book">
+        <table class="address_book__table">
+          <tr>
+            <th class="address_book__table--header">Name</th>
+            <th class="address_book__table--header">Phone</th>
+            <th class="address_book__table--header">Address</th>
+          </tr>
+          <tr>
+            <td class="address_book__table--name">Name</td>
+            <td class="address_book__table--tel">Phone</td>
+            <td class="address_book__table--address">Address</td>
+          </tr>
+        </table>
+      </div>
     </div>
     <div class="app app__paint">
-        <div class="app__header-bar app__paint--header-bar">
-            <img class="app__header-bar--icon" src="./assets/icons/paint.png" alt="" />
-            <span class="app__header-bar--title">Paint</span>
-            <button class="app__header-bar--close">
-                <img src="./assets/icons/close-icon.png" alt="" />
-            </button>
+      <div class="app__header-bar app__paint--header-bar">
+        <img class="app__header-bar--icon" src="./assets/icons/paint.png" alt="" />
+        <span class="app__header-bar--title">Paint</span>
+        <button class="app__header-bar--close">
+          <img src="./assets/icons/close-icon.png" alt="" />
+        </button>
+      </div>
+      <div class="app__utility"></div>
+      <div class="paint">
+        <div class="paint__mode">Mode: Coloring</div>
+        <div class="paint__container">
+          <div class="paint__board"></div>
+          <div class="paint__btns">
+            <button class="paint__btn paint__btn--black">Black</button>
+            <button class="paint__btn paint__btn--eraser">Erasor</button>
+            <button class="paint__btn paint__btn--gray">Gray</button>
+            <button class="paint__btn paint__btn--random">Random</button>
+            <button class="paint__btn paint__btn--reset">Reset</button>
+          </div>
         </div>
-        <div class="app__utility"></div>
-        <div class="paint">
-            <div class="paint__mode">Mode: Coloring</div>
-            <div class="paint__container">
-                <div class="paint__board"></div>
-                <div class="paint__btns">
-                    <button class="paint__btn paint__btn--black">Black</button>
-                    <button class="paint__btn paint__btn--eraser">Erasor</button>
-                    <button class="paint__btn paint__btn--gray">Gray</button>
-                    <button class="paint__btn paint__btn--random">Random</button>
-                    <button class="paint__btn paint__btn--reset">Reset</button>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-</body>
-
+  </body>
 </html>
